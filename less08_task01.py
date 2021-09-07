c1ce9d9032aabc7588d5206d65703ea7f151653b
Borodarus <_boroda_@mail.ru>
2021-09-08 01:46:27 +0300

    add less08 task01 solutions

--- less08_task01.py
+++ less08_task01.py
@@ -4,3 +4,26 @@
 """
 
 
+import hashlib
+
+s = 'Process finished with exit code 0'
+dictionary = {}
+count = 0
+
+for i in range(1, len(s)):
+    for j in range(len(s) - i + 1):
+        hsh = hashlib.sha1(s[j: j + i].encode('utf-8')).hexdigest()
+        
+        if dictionary.get(hsh) is not None:
+            dictionary[hsh] += 1
+        else:
+            dictionary.setdefault(hsh, 1)
+        count += 1
+        
+print(f'Всего подстрок: {count}')
+print(f'Уникальных подстрок: {len(dictionary)}\n')
+
+print('Повторяющиеся подстроки: ')
+for k, v in dictionary.items():
+    if v > 1:
+        print(f'{k}: {v}')
--- less08_task02.py
+++ less08_task02.py
@@ -3,3 +3,22 @@
 """
 
 
+s = 'Process finished with exit code 0'
+dictionary = {}
+
+for i in range(len(s)):
+    n = s[i: len(s) - (len(s) - i) + 1]
+    if dictionary.get(n) is not None:
+        dictionary[n] += 1
+    else:
+        dictionary.setdefault(n, 1)
+
+
+
+
+i = 0
+while i <= len(dictionary):
+    for k, v in dictionary.items():
+        if v == i + 1:
+            print(f'{k}: {v}')
+    i += 1
