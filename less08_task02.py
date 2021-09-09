b076211e3ab26c33a694141f5a28b474f8423a0f
Borodarus <_boroda_@mail.ru>
2021-09-10 00:58:00 +0300

    upd less08 task02 solutions

--- less08_task02.py
+++ less08_task02.py
@@ -8,42 +8,45 @@ from collections import Counter
 from collections import namedtuple
 
 
-# объвление класса для хранения информации о вершинах
+# информация о вершинах
 class Node(namedtuple('Node', ['left', 'right'])):
     def walk(self, code, acc):
-
         self.left.walk(code, acc + '0')
         self.right.walk(code, acc + '1')
 
 
-# объвление класса для хранения информации о листьях
+# информация о листьях
 class Leaf(namedtuple('Leaf', ['char'])):
     def walk(self, code, acc):
         code[self.char] = acc or '0'
 
 
 # encode
-def huffman_encode(s):
+def encode(s):
     h = []
     for ch, freq in Counter(s).items():
         h.append((freq, len(h), Leaf(ch)))
     heapq.heapify(h)
     count = len(h)
-    while len(h) > 1:
-        freq1, _count1, left = heapq.heappop(h)
-        freq2, _count2, right = heapq.heappop(h)
 
+    while len(h) > 1:
+        freq1, count1, left = heapq.heappop(h)
+        freq2, count2, right = heapq.heappop(h)
         heapq.heappush(h, (freq1 + freq2, count, Node(left, right)))
         count += 1
+
     code = {}
-    if h:
-        [(_freq, _count, root)] = h
+    if h != '':
+        [(freq, count, root)] = h
         root.walk(code, '')
+
+    # for ch in sorted(code):
+    #     print(f'{ch}: {code[ch]}')
     return code
 
 
 # decode
-def huffman_decode(encoded, code):
+def decode(encoded, code):
     sx = []
     enc_ch = ''
     for ch in encoded:
@@ -57,13 +60,10 @@ def huffman_decode(encoded, code):
 
 
 # main
-s = 'Process finished with exit code 0'
-code = huffman_encode(s)
+s = 'Do you like Python?'
+code = encode(s)
 encoded = ''.join(code[ch] for ch in s)
 
-print(len(code), len(encoded))
-
-for ch in sorted(code):
-    print(f'{ch}: {code[ch]}')
-print(encoded)
-print(huffman_decode(encoded, code))
+print(f'{s =}')
+print(f'{encoded =}')
+print(f'decode = {decode(encoded, code)}')
